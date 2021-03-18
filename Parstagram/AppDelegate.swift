14523421209b4049f891330e2c77ad02ecd3e04b
Hassan Amjad <amjadhassan535@gmail.com>
2021-03-18 19:38:49 -0400

    "Uploading Files From Xcode"

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import Parse
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,7 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        return true
+       	 	let parseConfig = ParseClientConfiguration {
+			$0.applicationId = "W8nb1tsCbWk0UiPQfEpbBPrJ7hykTsFzstHvbewV" // <- UPDATE 
+			$0.clientKey = "kwB7hmiLmKnvlurEMGupsHefHH4CngOE8EemPoE3" // <- UPDATE
+			$0.server = "https://parseapi.back4app.com"
+	}
+	Parse.initialize(with: parseConfig)
+	 return true
     }
 
     // MARK: UISceneSession Lifecycle
